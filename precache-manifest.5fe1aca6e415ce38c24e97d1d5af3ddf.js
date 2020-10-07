f4dc0f83ec59974439cec520861bc1d8319934e9
Arfath450 <46299355+Arfath450@users.noreply.github.com>
2020-10-07 09:46:21 +0530

    Updates

---
+++ precache-manifest.5fe1aca6e415ce38c24e97d1d5af3ddf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea073f2dfdf8538dff055c8fda0311d0",
+    "url": "/personal/index.html"
+  },
+  {
+    "revision": "bed01a4416d8c936e69c",
+    "url": "/personal/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "4ed6db890db7b129bf9d",
+    "url": "/personal/static/css/main.fa97df94.chunk.css"
+  },
+  {
+    "revision": "bed01a4416d8c936e69c",
+    "url": "/personal/static/js/2.497bf1ed.chunk.js"
+  },
+  {
+    "revision": "4e5653c2aea1f8f01a4afdf4252ee778",
+    "url": "/personal/static/js/2.497bf1ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ed6db890db7b129bf9d",
+    "url": "/personal/static/js/main.c8f66e0b.chunk.js"
+  },
+  {
+    "revision": "4e9c853d29b1df4846b9",
+    "url": "/personal/static/js/runtime-main.b0a78c69.js"
+  },
+  {
+    "revision": "560615fc5954a6d7946db219a0e65bf6",
+    "url": "/personal/static/media/img1.560615fc.jpg"
+  }
+]);
